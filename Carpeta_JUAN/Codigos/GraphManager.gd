5f191b40778a1b11c8e0cc87a8a353eb375bd220
Juan Felipe Enriquez Erazo <jfenriqueze@gmail.com>
2025-11-24 22:54:25 -0500

    Mas avances

--- Carpeta_JUAN/Codigos/GraphManager.gd
+++ Carpeta_JUAN/Codigos/GraphManager.gd
@@ -1,62 +1,71 @@
-extends Node
+extends Node2D
 
-var nodes := {}
-var edges := []
-var current_path := []
-var total_flow := 0
+@onready var nodes_container = $Nodes
+@onready var edges_container = $Edges
+
+var selected_nodes: Array[String] = []
+var edges: Dictionary = {}    # "A->B" : Edge (Line2D)
+
 
 func _ready():
-	load_graph()
+	_load_edges()
+	_connect_node_signals()
 
-func load_graph():
-	for node in get_node("Nodes").get_children():
-		nodes[node.node_id] = node
-		node.node_clicked.connect(self._on_node_clicked)
 
-	for edge in get_node("Edges").get_children():
-		edges.append(edge)
+# -----------------------------------------------------
+# CARGAR ARISTAS YA EXISTENTES
+# -----------------------------------------------------
+func _load_edges():
+	for edge in edges_container.get_children():
+		if edge is Line2D:
+			var key = "%s->%s" % [edge.from_id, edge.to_id]
+			edges[key] = edge
+			_update_edge_visual(edge)
 
-func _on_node_clicked(id):
-	if current_path.is_empty() and id != "S":
-		return
-	current_path.append(id)
-	highlight_path()
+# -----------------------------------------------------
+# CONECTAR CLICS DE NODOS
+# -----------------------------------------------------
+func _connect_node_signals():
+	for node in nodes_container.get_children():
+		if node.has_signal("node_clicked"):
+			node.connect("node_clicked", Callable(self, "_on_node_clicked"))
 
-	if id == "T":
-		process_path()
 
-func highlight_path():
-	# Aquí puedes pintar los nodos/aristas seleccionados
-	pass
+func _on_node_clicked(id: String):
+	selected_nodes.append(id)
+	if selected_nodes.size() == 2:
+		var a = selected_nodes[0]
+		var b = selected_nodes[1]
+		_highlight_if_edge_exists(a, b)
+		selected_nodes.clear()
 
-func process_path():
-	var min_residual = 99999
-	var valid = true
+# -----------------------------------------------------
+# RESALTAR ARISTA SI EXISTE
+# -----------------------------------------------------
+func _highlight_if_edge_exists(a: String, b: String):
+	var key1 = "%s->%s" % [a, b]
+	var key2 = "%s->%s" % [b, a]
+	var edge = edges.get(key1, null)
+	if edge == null:
+		edge = edges.get(key2, null)
+	if edge:
+		edge.default_color = Color.ORANGE_RED
+		edge.width = 6
+	else:
+		print("No hay arista entre ", a, " y ", b)
 
-	for i in range(current_path.size() - 1):
-		var e = get_edge(current_path[i], current_path[i+1])
-		if e == null or e.residual() <= 0:
-			valid = false
-			break
-		min_residual = min(min_residual, e.residual())
-
-	if not valid:
-		print("ERROR: camino inválido.")
-		current_path.clear()
-		return
-
-	# Enviar flujo
-	for i in range(current_path.size() - 1):
-		var e = get_edge(current_path[i], current_path[i+1])
-		e.push(min_residual)
-
-	total_flow += min_residual
-	print("Flujo total:", total_flow)
-
-	current_path.clear()
-
-func get_edge(a, b):
-	for e in edges:
-		if e.from_id == a and e.to_id == b:
-			return e
-	return null
+# -----------------------------------------------------
+# DIBUJAR ARISTA SEGÚN POSICIÓN DE NODOS
+# -----------------------------------------------------
+func _update_edge_visual(edge: Line2D):
+	var from_node = nodes_container.get_node(edge.from_id)
+	var to_node = nodes_container.get_node(edge.to_id)
+	if from_node and to_node:
+		edge.points = [
+			from_node.position,
+			to_node.position
+		]
+		edge.default_color = Color.SKY_BLUE
+		edge.width = 4
+	else:
+		print("Error: nodo no encontrado para arista ", edge.from_id, " -> ", edge.to_id)
